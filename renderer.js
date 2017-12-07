772627da49a6525a1431961da02f620c0b65aca9
Ajey Charantimath <ajey.charantimath@gmail.com>
2017-12-07 12:46:46 +0530

    demo code for causing crash

--- renderer.js
+++ renderer.js
@@ -1,3 +1,13 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+const {desktopCapturer} = require('electron');
+
+function pollForDesktopCapturer() {
+    desktopCapturer.getSources({types: ['window', 'screen']}, function(error, sources) {
+        console.log("Got sources - ", sources);
+        setInterval(pollForDesktopCapturer, 5000);
+    });
+}
+
+pollForDesktopCapturer();
